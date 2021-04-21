08afd888de69688949ff302d5ae3e226adc71335
陈之罕 <wb-czh931449@cainiao.com>
2021-04-21 10:40:05 +0800

    提交更新

--- downtool/gui.py
+++ downtool/gui.py
@@ -1,6 +1,22 @@
-# from dearpygui import core, simple
+from dearpygui import core, simple
+from downtool import down as dt
 
-import requests
-import dearpygui 
-print(dir(dearpygui))
-print(dir(requests))
+def save_callback(sender, data):
+    print("Save Clicked")
+
+with simple.window("name"):
+    core.add_button("Save", callback=save_callback)
+    core.add_input_text("string")
+    core.add_slider_float("float")
+
+# core.start_dearpygui()
+
+
+class Gui(dt):
+    def __init__(self):
+        super(Gui, self).__init__()
+        self.window = simple.window
+        self.core = core
+        self.simple = simple
+    
+    
